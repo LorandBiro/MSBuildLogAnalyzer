9cbcfde79635b6870fc50d8c4d7ee051417219e9
Loránd Biró <lorandbiro@outlook.com>
2016-04-18 23:24:30 +0200

    Increased version to 1.0.1.

--- Source/MSBuildLogAnalyzer/Properties/AssemblyInfo.cs
+++ Source/MSBuildLogAnalyzer/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Windows;
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
